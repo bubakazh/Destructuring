228d56dda2c65be363c836c7c46c5b4fcce16e42
bubakazh <troymanansala@gmail.com>
2022-10-24 13:31:09 -0700

    destructuring init commit

---
+++ destructuring.js
@@ -0,0 +1,35 @@
+
+const cars = ['Tesla', 'Mercedes', 'Honda']
+const [ randomCar ] = cars
+const [ ,otherRandomCar ] = cars
+//Predict the output
+console.log(randomCar)
+// Tesla
+console.log(otherRandomCar)
+// Mercedes
+
+const employee = {
+    name: 'Elon',
+    age: 47,
+    company: 'Tesla'
+}
+const { name: otherName } = employee;
+//Predict the output
+// console.log(name);
+// error?
+console.log(otherName);
+// Elon
+
+const person = {
+    name: 'Phil Smith',
+    age: 47,
+    height: '6 feet'
+}
+const password = '12345';
+const { password: hashedPassword } = person;  
+//Predict the output
+console.log(password);
+//12345
+console.log(hashedPassword);
+// error
+// ! didn't throw an error, but instead we get "undefined"
